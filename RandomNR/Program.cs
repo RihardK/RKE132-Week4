cd928347ebd4735be6286436b7a8aadc3bae88f9
Rix <rihard.konnapere@tktk.ee>
2023-05-31 14:45:06 +0300

    add week 4 code

---
+++ RandomNR/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+
+int randomSum = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    int userRandomNum = rnd.Next(0, 14001);
+    randomSum = randomSum + userRandomNum;
+    Console.WriteLine($"My generated value is: {userRandomNum}");
+}
+Console.WriteLine($"Total of randoms : {randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿Random rnd = new Random();
+
+int cpuRandom;
+
+bool loopActive = true;
+cpuRandom = rnd.Next(1, 21); //Tõstsin loopist välja, et number püsiks sama ja seda oleks võimalik lõpuks ära arvata.
+
+while (loopActive)
+{
+    Console.WriteLine("Enter your guess (1-20):");
+    //Console.WriteLine($"Current value = {cpuRandom}");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Correct! You won!");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Incorrect. Try again.");
+    }
+}
+Console.WriteLine("Goodbye.");
